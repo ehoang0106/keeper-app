85f7288f0217d97e61c7308d4b0b1f38c6f8b8a9
Khoa Hoang <khoahoang1698@gmail.com>
2024-03-08 14:26:18 -0800

    update Notes.jsx

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,16 +1,6 @@
 import notes from "../notes";
 import Note from "./Note";
 
-// function createNote1 (noteItem) {
-//     return(
-//         <Note
-//             key={noteItem.key}
-//             title={noteItem.title}
-//             content={noteItem.content}
-//         />
-//     );
-
-// }
 
 const Notes = () => {
     return (  
