ffa0c2cf2908b2b6e76a8fbff80a45cb3d257873
ehoang0106 <khoahoang1698@gmail.com>
2024-03-24 18:50:21 -0700

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,34 @@
-import Footer from "./Footer";
-import Header from "./Header";
-import Note from "./Note"
-import Test from "./Test";
+// import Footer from "./Footer";
+// import Header from "./Header";
+// import Note from "./Note"
+// import Test from "./Test";
+
+import Card from "../components/Card";
+import contacts from "../contacts";
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <Note />
-            <Footer />
-            {/* <Test /> */}
+        <div>
+            <h1 className="heading">My Contacts</h1>
+
+            <Card
+                name={contacts[0].name}
+                img={contacts[0].imgURL}
+                tel={contacts[0].phone}
+                email={contacts[0].email}
+            />
+            <Card
+                name={contacts[1].name}
+                img={contacts[1].imgURL}
+                tel={contacts[1].phone}
+                email={contacts[1].email}
+            />
+            <Card
+                name={contacts[2].name}
+                img={contacts[2].imgURL}
+                tel={contacts[2].phone}
+                email={contacts[2].email}
+            />
         </div>
     );
 }
